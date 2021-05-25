79ed809ae73923931cf35bb802d6cf6fdd603f23
李清龙 <924229679@qq.com>
2021-05-25 22:20:19 +0800

    二叉树的层序遍历

---
+++ 数据结构/二叉树/层序遍历二叉树/层序遍历二叉树.js
@@ -0,0 +1,41 @@
+// 利用队列实现二叉树的层序遍历
+
+const levelOrder = (root) => {
+  if (!root) return;
+
+  const queue = [root];
+  const result = [];
+
+  while (queue.length) {
+    const size = queue.length;
+    const vec = [];
+
+    for (let i = 0; i < size; i++) {
+      const node = queue.shift();
+      vec.push(node.data);
+      if (node.left) queue.push(node.left);
+      if (node.right) queue.push(node.right);
+    }
+
+    result.push(vec);
+  }
+
+  return result;
+};
+
+// 二叉树对象
+const node = {
+  data: 10,
+  left: {
+    data: 9,
+    left: { data: 7, left: null, right: null },
+    right: { data: 8, left: null, right: null },
+  },
+  right: {
+    data: 11,
+    left: { data: 10, left: null, right: null },
+    right: { data: 12, left: null, right: null },
+  },
+};
+
+console.log(levelOrder(node));
